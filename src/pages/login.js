e72bd393bca9f44e8af490ca762b851b1667f1e5
Eco Galileo <ericogalileo@gmail.com>
2021-08-23 19:12:34 +0800

    added background image and change the color scheme

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import HomeNavbar from "../components/HomeNavbar";
+// import HomeNavbar from "../components/HomeNavbar";
 
 const login = () => {
   return (
     <>
-      <HomeNavbar />
+      {/* <HomeNavbar /> */}
       <div class='min-h-screen bg-gray-200 flex items-center'>
         <div class='container mx-auto max-w-md shadow-md hover:shadow-lg transition duration-300'>
           <div class='py-12 p-10 bg-white rounded-xl'>
             <div class='flex justify-center mb-4'>
-              <i class='far fa-user-circle text-8xl'></i>
+              <Link to='/'>
+                <img
+                  src={
+                    "https://www.feu.edu.ph/wp-content/themes/feu_theme2019/assets/images/logo_white2.png"
+                  }
+                  alt='Logo'
+                  className='h-full rounded mb-1 shadow'
+                ></img>
+              </Link>
             </div>
             <div class='mb-6'>
               <label
@@ -21,7 +29,7 @@ const login = () => {
               </label>
               <input
                 type='text'
-                class='border bg-gray-100 py-2 px-4 w-full outline-none focus:ring-2 focus:ring-indigo-400 rounded'
+                class='border bg-gray-100 py-2 px-4 w-full outline-none focus:ring-2 focus:ring-yellow-300 rounded'
                 placeholder='Your username'
               />
             </div>
@@ -34,7 +42,7 @@ const login = () => {
               </label>
               <input
                 type='text'
-                class='border bg-gray-100 py-2 px-4 w-full outline-none focus:ring-2 focus:ring-indigo-400 rounded'
+                class='border bg-gray-100 py-2 px-4 w-full outline-none focus:ring-2 focus:ring-yellow-300 rounded'
                 placeholder='Your password'
               />
             </div>
@@ -42,7 +50,7 @@ const login = () => {
               Forgot password
             </Link>
             <Link to='/AdminDashboard'>
-              <button class='w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300'>
+              <button class='w-full mt-6 text-yellow-300 font-bold bg-green-600 py-3 rounded-md hover:bg-green-700 transition duration-300'>
                 LOGIN
               </button>
             </Link>
