22b542534145c3dc4f7c836622b547637a7ec8ce
Genesis Gabiola <gbgabiola@gmail.com>
2021-08-24 09:03:17 +0800

    Update Students add edit and delete

--- src/components/Students/AddStudent.js
+++ src/components/Students/AddStudent.js
@@ -1,7 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import AdminSidebar from '../../pages/AdminSidebar';
 
 const AddStudent = () => {
-  return <div></div>;
+  let history = useHistory();
+  const [student, setStudent] = useState({
+    name: '',
+    course: '',
+    major: '',
+    email: '',
+    address: '',
+    image: '',
+  });
+
+  const { name, course, email, address, image } = student;
+  const onInputChange = e => {
+    setStudent({ ...student, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.post('http://localhost:3003/students', student);
+    history.push('/students');
+  };
+  return (
+    <div className="relative md:flex">
+      <AdminSidebar />
+      {/* Content Start */}
+      <div class="flex-1 p-10 text-2xl font-bold">
+        <div className="relative flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              {/* <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"></div> */}
+
+              <div className="">
+                <div className="">
+                  <h2 className="">Add A Student</h2>
+                  <form onSubmit={e => onSubmit(e)}>
+                    <div className="">
+                      <input
+                        type="text"
+                        className=""
+                        placeholder="Enter Your Name"
+                        name="name"
+                        value={name}
+                        onChange={e => onInputChange(e)}
+                      />
+                    </div>
+                    <div className="">
+                      <input
+                        type="text"
+                        className=""
+                        placeholder="Enter Your Course"
+                        name="course"
+                        value={course}
+                        onChange={e => onInputChange(e)}
+                      />
+                    </div>
+                    <div className="">
+                      <input
+                        type="email"
+                        className=""
+                        placeholder="Enter Your E-mail Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onInputChange(e)}
+                      />
+                    </div>
+                    <div className="">
+                      <input
+                        type="text"
+                        className=""
+                        placeholder="Enter Your Address"
+                        name="address"
+                        value={address}
+                        onChange={e => onInputChange(e)}
+                      />
+                    </div>
+                    <div className="">
+                      <input
+                        type="text"
+                        className=""
+                        placeholder="Enter Your Image"
+                        name="image"
+                        value={image}
+                        onChange={e => onInputChange(e)}
+                      />
+                    </div>
+                    <button className="">Add Student</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Content end */}
+      </div>
+    </div>
+  );
 };
 
 export default AddStudent;
--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -28,19 +28,20 @@ import { Link } from 'react-router-dom';
 // ];
 
 export default function Students() {
-  const [students, setStudents] = useState([]);
+  const [students, setStudent] = useState([]);
 
   useEffect(() => {
     loadStudents();
   }, []);
 
   const loadStudents = async () => {
-    const result = await axios.get('http://localhost:3006/students');
-    setStudents(result.data.reverse());
+    console.log('Data loaded');
+    const result = await axios.get('http://localhost:3003/students');
+    setStudent(result.data.reverse());
   };
 
   const removeStudent = async id => {
-    await axios.delete(`http://localhost:3006/students/${id}`);
+    await axios.delete(`http://localhost:3003/students/${id}`);
     loadStudents();
   };
 
@@ -52,8 +53,11 @@ export default function Students() {
         <div class="flex-1 p-10 text-2xl font-bold">
           <div className="flex flex-row-reverse">
             {' '}
-            <Link class="bg-blue-600 hover:bg-blue-800 rounded-md p-2 mb-8">
-              Add Students
+            <Link
+              class="bg-blue-600 text-white hover:bg-blue-800 rounded-md p-2 mb-8"
+              to="/students/add"
+            >
+              Add Student
             </Link>
           </div>
 
@@ -65,6 +69,12 @@ export default function Students() {
                   <table className="min-w-full divide-y divide-gray-200">
                     <thead className="bg-gray-50">
                       <tr>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          #
+                        </th>
                         <th
                           scope="col"
                           className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
@@ -95,8 +105,9 @@ export default function Students() {
                       </tr>
                     </thead>
                     <tbody className="bg-white divide-y divide-gray-200">
-                      {students.map(student => (
+                      {students.map((student, idx) => (
                         <tr key={student.email}>
+                          <th scope="row">{idx + 1}</th>
                           <td className="px-6 py-4 whitespace-nowrap">
                             <div className="flex items-center">
                               <div className="flex-shrink-0 h-10 w-10">
@@ -120,9 +131,6 @@ export default function Students() {
                             <div className="text-sm text-gray-900">
                               {student.course}
                             </div>
-                            <div className="text-sm text-gray-500">
-                              {student.major}
-                            </div>
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap">
                             <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
@@ -134,15 +142,21 @@ export default function Students() {
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium text-white">
                             <Link
+                              className="bg-blue-600 hover:bg-blue-700 rounded-md mr-4 p-2"
+                              to={`/students/${student.id}`}
+                            >
+                              <i class="fas fa-eye"></i> View
+                            </Link>
+                            <Link
                               type="button"
-                              className="bg-blue-600 hover:bg-blue-800 rounded-md mr-4 p-2"
+                              className="bg-yellow-600 hover:bg-yellow-700 rounded-md mr-4 p-2"
                               to={`/students/edit/${student.id}`}
                             >
                               <i class="fas fa-edit"></i> Edit
                             </Link>
                             <Link
                               type="button"
-                              className="bg-red-600 hover:bg-red-800 rounded-md p-2"
+                              className="bg-red-600 hover:bg-red-700 rounded-md p-2"
                               onClick={removeStudent(student.id)}
                             >
                               <i class="far fa-trash-alt"></i> Delete
