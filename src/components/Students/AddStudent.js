f91c3b457d21542a2c10b2b2ba916207c273512a
Justine Micarandayo <87022040+jmicarandayo@users.noreply.github.com>
2021-08-24 22:02:18 +0800

    Modify AddStudent data form

--- src/components/Students/AddStudent.js
+++ src/components/Students/AddStudent.js
@@ -7,13 +7,12 @@ const AddStudent = () => {
   let history = useHistory();
   const [student, setStudent] = useState({
     name: '',
-    course: '',
+    gradeLevel: '',
     email: '',
     address: '',
     image: '',
   });
-
-  const { name, course, email, address, image } = student;
+  const { name, gradeLevel, email, address, image } = student;
   const onInputChange = e => {
     setStudent({ ...student, [e.target.name]: e.target.value });
   };
@@ -27,67 +26,72 @@ const AddStudent = () => {
     <div className="relative md:flex">
       <AdminSidebar />
       {/* Content Start */}
-      <div class="flex-1 p-10 text-2xl font-bold">
+      <div class="flex-1 p-5 text-lg">
         <div className="relative flex flex-col">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
               {/* <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"></div> */}
 
-              <div className="">
-                <div className="">
-                  <h2 className="">Add A Student</h2>
+              <div className="grid xl:grid-cols-2">
+                <div className="container  bg-gray-300 md:w-9/12 p-6">
+                  <h2 className="text-green-600 font-semibold text-3xl mb-2">Add Student</h2>
                   <form onSubmit={e => onSubmit(e)}>
-                    <div className="">
+                  <div className="flex flex-col">
+                    <label className="mt-1 font-semibold">Full Name</label>
                       <input
                         type="text"
-                        className=""
-                        placeholder="Enter Your Name"
+                        className="rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 appearance-none border border-gray-300 px-2"
+                        placeholder="Name"
                         name="name"
                         value={name}
                         onChange={e => onInputChange(e)}
                       />
                     </div>
-                    <div className="">
+                    <div className="flex flex-col">
+                    <label className="mt-1 font-semibold">Grade Level</label>
                       <input
                         type="text"
-                        className=""
-                        placeholder="Enter Your Course"
-                        name="course"
-                        value={course}
+                        className="rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 appearance-none border border-gray-300 px-2"
+                        placeholder="Grade Level"
+                        name="gradeLevel"
+                        value={gradeLevel}
                         onChange={e => onInputChange(e)}
                       />
                     </div>
-                    <div className="">
+                    <div className="flex flex-col">
+                    <label className="mt-1 font-semibold">E-mail</label>
                       <input
                         type="email"
-                        className=""
-                        placeholder="Enter Your E-mail Address"
+                        className="rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 appearance-none border border-gray-300 px-2"
+                        placeholder="student@company.com"
                         name="email"
                         value={email}
                         onChange={e => onInputChange(e)}
                       />
                     </div>
-                    <div className="">
+                    <div className="flex flex-col">
+                    <label className="mt-1 font-semibold">Address</label>
                       <input
                         type="text"
-                        className=""
-                        placeholder="Enter Your Address"
+                        className="rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 appearance-none border border-gray-300 px-2"
+                        placeholder="Address"
                         name="address"
                         value={address}
                         onChange={e => onInputChange(e)}
                       />
                     </div>
-                    <div className="">
+                    <div className="flex flex-col">
+                    <label className="mt-1 font-semibold">Image</label>
                       <input
                         type="text"
-                        className=""
-                        placeholder="Enter Your Image"
+                        className="rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 appearance-none border border-gray-300 px-2"
+                        placeholder="Enter Image"
                         name="image"
                         value={image}
                         onChange={e => onInputChange(e)}
                       />
                     </div>
-                    <button className="">Add Student</button>
+                    <button className="bg-blue-800 hover:bg-blue-600 text-white py-1 px-4 md:w-5/12 mt-6 rounded-md">Add Student</button>
                   </form>
                 </div>
               </div>
